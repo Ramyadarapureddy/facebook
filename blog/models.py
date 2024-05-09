4aa9c3bbe2c7539f58dca18f1afad2eafb84d07c
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-05-09 16:37:35 +0530

    created post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from auth_manager.models import CustomUser
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, 
+                               on_delete=models.CASCADE)
+    title = models.TextField()
+    image = models.ImageField(upload_to='posts/images/',
+                              blank=True,null=True)
+    likes = models.ManyToManyField(CustomUser, related_name="my_liked_post")
+    posted_at = models.DateTimeField(auto_now_add=True)
+
