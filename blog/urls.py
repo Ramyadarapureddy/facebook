580b41a831dcb5f3450dd40953bae7f7293b66b1
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-14 18:05:49 +0530

    added  update-like path

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('timeline',views.timeline_page,name='timeline-page'),
     path('home',views.home_page,name='home-page'),
     path('create-post',views.create_post,name='create-post'),
+    path('update-like',views.update_like,name='update-like'),
 ]
