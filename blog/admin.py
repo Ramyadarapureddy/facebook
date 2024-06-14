2e3f4986cca3940a8eff6ca56d8e768b8c82b96e
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-14 18:04:36 +0530

    added PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 from . import models
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
 # Register your models here.
-admin.site.register(models.Post)
+admin.site.register(models.Post, PostAdmin)
