ef704002883e6545d27f6c923cc0996fc4934847
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-14 11:51:27 +0530

    added create-post url

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,8 @@
-urlpatterns=[]
+from django.urls import path
+
+from . import views
+urlpatterns=[
+    path('timeline',views.timeline_page,name='timeline-page'),
+    path('home',views.home_page,name='home-page'),
+    path('create-post',views.create_post,name='create-post'),
+]
