6464cf85d10bc601b158a09d8b17b97ad1064c18
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-18 18:17:53 +0530

    added CommentAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,9 @@ from . import models
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id','user','post','text')
 # Register your models here.
 admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment, CommentAdmin)
