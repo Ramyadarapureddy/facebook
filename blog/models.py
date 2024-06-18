338060b900e6b5da32232a69345ac325b9d0a51e
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-18 18:18:25 +0530

    comment updated

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,9 @@ class Comment(models.Model):
     text = models.TextField()
     commeted_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.text
+
 class Follow(models.Model):
     follower = models.ForeignKey(CustomUser,related_name="following", on_delete=models.CASCADE)
     following = models.ForeignKey(CustomUser,related_name="followers", on_delete=models.CASCADE)
