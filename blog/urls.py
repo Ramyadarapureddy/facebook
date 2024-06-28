95a7de9ab7b165cb358432ccbc5ad73929186b71
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-06-28 16:27:28 +0530

    added path for handle-follow

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('home',views.home_page,name='home-page'),
     path('create-post',views.create_post,name='create-post'),
     path('update-like',views.update_like,name='update-like'),
+    path('handle-follow/<int:user_id>',views.handle_follow,name='handle-follow'),
     path('add-comment/<int:post_id>', views.add_comment, name='add-comment'),
-    path('profile-page',views.profile_page,name='profile-page'),
+    path('profile-page/<int:user_id>',views.profile_page,name='profile-page'),
 ]
